b45b8ce262021916126ce837da5dd50bdae701d7
kkaltenheuser <karenkaltenheuser@gmail.com>
2020-11-08 14:44:20 -0500

    update tables

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,9 @@
+-- insert burgers into table --
 INSERT INTO burger (burger_name) VALUES ('Turkey Burger');
 INSERT INTO burger (burger_name) VALUES ('Veggie Burger');
 INSERT INTO burger (burger_name) VALUES ('Portobello Burger');
+INSERT INTO burger (burger_name) VALUES ('Quinoa Burger');
+INSERT INTO burger (burger_name) VALUES (' Salmon Burger ')
+-- burgers that have been consumed already --
+INSERT INTO burger (burger_name, devoured) VALUES ('Bison Burger', true);
+INSERT INTO burger (burger_name, devoured) VALUES ('Korean BBQ Burger', true);
