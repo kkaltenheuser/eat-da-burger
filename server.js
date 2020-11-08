601201ee7906c236ee2120e9ce8b33c94b96825d
kkaltenheuser <karenkaltenheuser@gmail.com>
2020-11-08 14:23:24 -0500

    setup all folders and insert files

--- server.js
+++ server.js
@@ -1,33 +1,37 @@
-var express = require("express");
-var exphbs = require("express-handlebars");
-var mysql = require("mysql");
+// dependencies
 
+// Require dotenv
+require("dotenv").config();
+
+// Require express
+var express = require("express");
+
+// Set-up + initiate PORT
+var PORT = process.env.PORT || 3000;
+
+// Invoke express
 var app = express();
 
-// Set the port of our application
-// process.env.PORT lets the port be set by Heroku
-var PORT = process.env.PORT || 8080;
+// static content for the app from the "public" directory
+app.use(express.static("public"));
 
-// Parse request body as JSON
+// Parse request as JSON
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+// Set Handlebars - express
+var exphbs = require("express-handlebars");
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "100084896",
-  database: "movie_planner_db",
-});
+// Import and give the server access to routes
+var routes = require("./controllers/burgerController.js");
 
-connection.connect(function (err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
+// use all routes
+app.use(routes);
 
-  console.log("connected as id " + connection.threadId);
+// listen to port and respond
+app.listen(PORT, function () {
+  console.log("App now listening at localhost:" + PORT);
 });
