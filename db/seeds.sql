df7c0c8b41467f78dc63e1c15f4e9cb257ffe34c
kkaltenheuser <karenkaltenheuser@gmail.com>
2020-12-05 20:36:04 -0500

    select from for database

--- db/seeds.sql
+++ db/seeds.sql
@@ -7,3 +7,5 @@ INSERT INTO burgers (burger_name) VALUES (' Salmon Burger ');
 -- burgers that have been consumed already --
 INSERT INTO burgers (burger_name, devoured) VALUES ('Bison Burger', true);
 INSERT INTO burgers (burger_name, devoured) VALUES ('Korean BBQ Burger', true);
+
+SELECT * FROM burgers
