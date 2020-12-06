383698b5e7d820d2cb15b538746e809871e78835
kkaltenheuser <karenkaltenheuser@gmail.com>
2020-12-05 20:04:02 -0500

    clarify comments

--- db/schema.sql
+++ db/schema.sql
@@ -1,13 +1,14 @@
--- Drops the day_planner_db if it already exists --
+-- Drops the burger_db if it already exists --
 DROP DATABASE IF EXISTS burger_db;
 
--- Create the database day_planner_db and specified it for use.
+-- Create the database burger_db and specified it for use.
 CREATE DATABASE burger_db;
 
 USE burger_db;
 
 -- Create the table plans.
 CREATE TABLE burgers (
+  
   id int NOT NULL AUTO_INCREMENT,
   burger_name varchar(300) NOT NULL,
   devoured BOOLEAN DEFAULT false,
